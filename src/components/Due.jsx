45d704dd1d9b2facc60c6bcf8faf1e01e6f67e0d
Azhar <azhar.subair.2@gmail.com>
2026-06-16 02:09:33 +0530

    first commit

--- src/components/Due.jsx
+++ src/components/Due.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Trial() {
     return (
-        <div className="flex justify-center text-yellow-400 fixed top-0 left-0 w-full z-50 py-2 bg-black/50 font-bold text-sm backdrop-blur-md">
+        <div className="flex justify-center text-yellow-400 fixed top-0 left-0 w-full z-50 py-2 bg-black/50 font-bold text-sm backdrop-blur-md text-2xl">
             Trial Version : This is a trial version of the application. Some features may be limited or unavailable. Please upgrade to the full version for complete access.
         </div>
     )
